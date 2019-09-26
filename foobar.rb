863d6cbf941810a53249f707068e96c7ba0255a1
apa915436 <apa915436@gmail.com>
2019-09-25 19:56:55 -0700

    hw3

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,8 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a = a.collect{|e| e.to_i + 2}
+    a = a.select{|e| e < 10 && e.even?}.uniq
+    a.sum()
   end
 end
